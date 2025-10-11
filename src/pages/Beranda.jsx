c816824de661b25c8fd1d36185b7426cf62a03a1
Putra Yonathan <putrayonathan88@gmail.com>
2025-10-11 13:03:36 +0900

    Update

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -11,9 +11,10 @@ export default function Beranda() {
   return (
     <div className="home-container">
       <div className="home-content">
+        <h1 className='home-text'>Mekar Tanjung</h1>
         <h1 className="home-title">Kami Ada Karena Rasa</h1>
         <p className="home-subtitle">
-          Segarkan harimu dengan jus buah pilihan terbaik!
+          Segarkan harimu, hatimu dengan jus buah pilihan terbaik!
         </p>
         <button className="home-button" onClick={goToProducts}>
           Lihat Menu Kami
